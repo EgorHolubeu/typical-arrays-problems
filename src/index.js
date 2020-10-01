7c1f6f9df1724dd210116663a4228e8c9b6aff53
Egor <egorgolubev12@gmail.com>
2020-10-01 21:15:29 +0300

    feat: calculate max and average value

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return array.length ? array.sort((a, b) => a - b)[0] : 0;
+  return arguments.length ? array.sort((a, b) => a - b)[0] : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return array.sort((a, b) => b - a)[0] || 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array.reduce((p, c) => p + c)/array.length || 0;
 }
